3145fdf52955a1e5a0935c55a651cc7797092271
wardy30 <wardydev@gmail.com>
2023-12-29 23:47:44 +0800

    add api data process

---
+++ src/store/services/prepareHeader.ts
@@ -0,0 +1,14 @@
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const baseQuery = fetchBaseQuery({
+    baseUrl: 'https://apiliquid8.digitalindustryagency.com/api',
+    //   prepareHeaders: (headers) => {
+    //     const token = localStorage.getItem('token');
+
+    //     if (token) {
+    //       headers.set('authorization', `Bearer ${token}`);
+    //     }
+
+    //     return headers;
+    //   },
+});
--- src/store/services/types.ts
+++ src/store/services/types.ts
@@ -21,4 +21,66 @@ interface UserDataItem {
         bs: string;
     };
 }
-export type { UserDataItem };
+interface GenerateInboundDataProcessResponse {
+    data: {
+        status: boolean;
+        message: string;
+        resource: {
+            code_document: string;
+            headers: any;
+            file_name: string;
+            templateHeaders: string[];
+            fileDetails: {
+                total_column_count: number;
+                total_row_count: number;
+            };
+        };
+    };
+}
+
+interface ProductOlds {
+    data: {
+        status: boolean;
+        message: string;
+        resource: {
+            current_page: number;
+            data: {
+                id: number;
+                code_document: string;
+                old_barcode_product: string;
+                old_name_product: string;
+                old_quantity_product: string;
+                old_price_product: string;
+                created_at: string;
+                updated_at: string;
+            }[];
+        };
+    };
+}
+interface MergeHeader {
+    data: {
+        status: boolean;
+        message: string;
+        resource: {
+            code_document: string;
+            old_barcode_product: string;
+            old_name_product: string;
+            old_quantity_product: number;
+            old_price_product: number;
+            updated_at: string;
+            created_at: string;
+            id: number;
+        };
+    };
+}
+interface MergeHeaderBody {
+    code_document: string | undefined;
+    headerMappings: {
+        old_barcode_product: string[] | undefined;
+        old_name_product: string[] | undefined;
+        old_quantity_product: string[] | undefined;
+        old_price_product: string[] | undefined;
+    };
+}
+
+export type { UserDataItem, GenerateInboundDataProcessResponse, ProductOlds, MergeHeader, MergeHeaderBody };
