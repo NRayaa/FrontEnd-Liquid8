eb3b5d21c3cb0aab2d86a96a73091d21ca200786
lusipr <lusipr142@gmail.com>
2024-02-09 09:14:53 +0700

    set name & email profile

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -115,6 +115,7 @@ const Header = () => {
     const [flag, setFlag] = useState(themeConfig.locale);
 
     const { t } = useTranslation();
+    const userProfile = JSON.parse(localStorage.getItem('profile') || '{}');
 
     const handleSignout = () => {
         localStorage.removeItem('token');
@@ -522,11 +523,11 @@ const Header = () => {
                                             <img className="rounded-md w-10 h-10 object-cover" src="/assets/images/user-profile.jpeg" alt="userProfile" />
                                             <div className="ltr:pl-4 rtl:pr-4 truncate">
                                                 <h4 className="text-base">
-                                                    John Doe
+                                                    {userProfile.name}
                                                     <span className="text-xs bg-success-light rounded text-success px-1 ltr:ml-2 rtl:ml-2">Pro</span>
                                                 </h4>
                                                 <button type="button" className="text-black/60 hover:text-primary dark:text-dark-light/60 dark:hover:text-white">
-                                                    johndoe@gmail.com
+                                                    {userProfile.email}
                                                 </button>
                                             </div>
                                         </div>
