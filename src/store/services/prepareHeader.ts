747f463d206a16eb76234d7c0788358290267ab7
lusipr <lusipr142@gmail.com>
2024-02-22 10:39:29 +0700

    edit base url

--- src/store/services/prepareHeader.ts
+++ src/store/services/prepareHeader.ts
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://laluisari.payon.id/api',
+    baseUrl: 'http://server.wms-liquid8.online/api',
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
 
