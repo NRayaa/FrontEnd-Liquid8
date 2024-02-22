8d4676dc309e2de30b3e88d8f9ccdac1fcf4722a
shiqocred <21103080090@student.uin-suka.ac.id>
2024-02-22 08:52:13 +0700

    change header

--- src/store/services/prepareHeader.ts
+++ src/store/services/prepareHeader.ts
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://server.wms-liquid8.online/api',
+    baseUrl: 'https://laluisari.payon.id/api',
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
 
