deecc69a6cf1c84f3e87ec5f140b8fb25a888ded
lusipr <lusipr142@gmail.com>
2024-08-28 11:36:53 +0700

    fixs bug back

--- src/pages/Inbound/CheckProduct/ApprovementProduct/DetailApproveDocument.tsx
+++ src/pages/Inbound/CheckProduct/ApprovementProduct/DetailApproveDocument.tsx
@@ -88,22 +88,22 @@ const DetailApproveDocument = () => {
 
     useEffect(() => {
         if (isSuccess && data.data.status) {
-            setListsData(data.data.resource);
+            setListsData(data?.data?.resource);
         }
         refetch();
     }, [data, isSuccess, refetch]);
 
     useEffect(() => {
         if (results.isSuccess) {
-            toast.success(results.data.data.message);
+            toast.success(results?.data?.data?.message);
             refetch();
         } else if (results.isError) {
-            toast.error(results.data.data.message);
+            toast.error(results?.data?.data?.message);
         }
     }, [results, refetch]);
 
     if (isError && !data?.data?.status) {
-        return <Alert message={data?.data.message ?? 'anda tidak berhak mengakses halaman ini'} />;
+        return <Alert message={data?.data?.message ?? 'anda tidak berhak mengakses halaman ini'} />;
     }
 
     return (
@@ -126,7 +126,7 @@ const DetailApproveDocument = () => {
                 <h1 className="text-lg font-bold flex justify-start py-4">LIST DATA DOCUMENT</h1>
                 <div className="flex md:items-center md:flex-row flex-col mb-5 gap-5">
                     {/* add button back */}
-                    <Link to="/inbound/check_product/approvment_document">
+                    <Link to="/inbound/check_product/approvment_product">
                         <button type="button" className=" px-2 btn btn-outline-danger">
                             <IconArrowBackward className="flex mx-2" fill={true} /> Back
                         </button>
