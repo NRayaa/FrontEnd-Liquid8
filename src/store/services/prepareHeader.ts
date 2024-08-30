51dce4cfd4f08765f3ce485ef9b547223c0633b3
shiqocred <21103080090@student.uin-suka.ac.id>
2024-08-30 11:07:19 +0700

    add

--- src/store/services/prepareHeader.ts
+++ src/store/services/prepareHeader.ts
@@ -1,9 +1,13 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// development
+export const baseUrl = 'https://wms-server.digitalindustryagency.com/api';
+
+// production
+// export const baseUrl = 'https://server.wms-liquid8.online/api'
+
 export const baseQuery = fetchBaseQuery({
-    // baseUrl: 'https://server.wms-liquid8.online/api',
-    baseUrl: 'https://wms-server.digitalindustryagency.com/api',
-    // baseUrl: 'http://127.0.0.1:8000/api',
+    baseUrl: baseUrl,
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
 
