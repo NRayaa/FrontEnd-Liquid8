c642db9056f6038648547d8292615cbce83fce32
lusipr <lusipr142@gmail.com>
2024-10-01 09:41:55 +0700

    clean code

--- src/pages/Inbound/BulkingProduct/BulkingProduct.tsx
+++ src/pages/Inbound/BulkingProduct/BulkingProduct.tsx
@@ -136,13 +136,7 @@ const BulkingProduct = () => {
                                 </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
-                                {/* {errorResources.map((resource, index) => (
-                                    <tr key={index}>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{resource}</td>
-                                    </tr>
-                                ))} */}
-
-                                {errorResources.map((resource, index) => (
+                           {errorResources.map((resource, index) => (
                                     <tr key={index}>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{JSON.stringify(resource || "")}</td>
                                     </tr>
